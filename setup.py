8fb6684ed9b28c57e1cb85adc5b1eb005e0c2115
shreyashettyu <shreyashettyu2@gmail.com>
2024-10-19 00:36:00 +0100

    Added requirement packages and setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us-visa",
+    version="0.0.0",
+    author="shreya",
+    author_email="shreyashettyu2@gmail.com",
+    packages=find_packages()
+)
